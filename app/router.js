f841316cfd8e2f8ef266ebf6595703fdf305f5a3
ghostxbh <ghostxbh@hotmail.com>
2019-08-27 11:53:37 +0800

    update router

--- app/router.js
+++ app/router.js
@@ -7,12 +7,12 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
 
-  // news domain
-  router.get('/news', controller.news.index);
-  router.get('/news/list', controller.news.list);
-  router.post('/news/add', controller.news.create);
-  router.post('/news/update', controller.news.update);
-  router.get('/news/del/:_id', controller.news.remove);
-
-
+  // user operation
+  router.get('/user/load', controller.user.insertData);
+  router.get('/user/list', controller.user.find);
+  router.post('/user', controller.user.create);
+  router.delete('/user/:_id', controller.user.delete);
+  router.put('/user', controller.user.update);
+  router.get('/user/:_id', controller.user.findOne);
+  router.get('/user/', controller.user.findPage);
 };
