bad80ee6571b61381a6800d5569ec7cca5be21f6
ghostxbh <ghostxbh@hotmail.com>
2019-08-28 09:12:21 +0800

    modify helper

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,8 +1,22 @@
 "use strict";
 const Crypto = require('crypto');
 
+/**
+ * helper
+ * @type {{encrypt(*=): *, success(*, *, *=, *=): *, error(*, *=, *=, *=): *}}
+ */
 module.exports = {
     encrypt(pwd) {
         return Crypto.createHmac('sha1', this.config.pwd_salt).update(pwd).digest('hex');
-    }
+    },
+    success(ctx, data, code = 200, message = 'ok') {
+        return ctx.body = {
+            code, message, data
+        }
+    },
+    error(ctx, message = 'error', code = 500, data = null) {
+        return ctx.body = {
+            code, message, data,
+        }
+    },
 };
