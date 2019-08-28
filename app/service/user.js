50e21cb9a34664fe08c8e6f75a946d483107e1e7
ghostxbh <ghostxbh@hotmail.com>
2019-08-28 09:12:41 +0800

    update user service

--- app/service/user.js
+++ app/service/user.js
@@ -1,5 +1,8 @@
 const Service = require('egg').Service;
 
+/**
+ * user service crud
+ */
 class UserService extends Service {
     async create(user) {
         const {ctx} = this;
@@ -12,14 +15,43 @@ class UserService extends Service {
         return await ctx.model.User.delete({_id: id});
     }
 
-    async update(user) {
+    async update(id, user) {
         const {ctx} = this;
         user.password = ctx.helper.encrypt(user.password);
-        return await ctx.model.User.update(user);
+        return await ctx.model.User.update({'_id': id}, user);
     }
 
-    async find(id) {
+    async findOne(id) {
         const {ctx} = this;
-        return await ctx.model.User.find({_id: id});
+        let [data] = await ctx.model.User.find({_id: id});
+        return data;
+    }
+
+    async find(param) {
+        const {ctx} = this;
+        return await ctx.model.User.find(param, {
+            _id: 1,
+            name: 1,
+            age: 1,
+            gender: 1,
+            phone: 1,
+            email: 1,
+            create_time: 1,
+        });
+    }
+
+    async findPage(page, size) {
+        const {ctx} = this;
+        return await ctx.model.User.find({}, {
+            _id: 1,
+            name: 1,
+            age: 1,
+            gender: 1,
+            phone: 1,
+            email: 1,
+            create_time: 1,
+        }).skip(page * size).limit(size);
     }
 }
+
+module.exports = UserService;
