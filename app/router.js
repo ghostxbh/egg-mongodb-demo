c76c471d29633faca2cb8983bc2f1e058bcdf8fe
ghostxbh <ghostxbh@hotmail.com>
2019-11-28 09:22:00 +0800

    add uuid & cellection router

--- app/router.js
+++ app/router.js
@@ -13,7 +13,16 @@ module.exports = app => {
     router.get('/user/list', controller.user.find);
     router.post('/user', controller.user.create);
     router.delete('/user/:_id', controller.user.delete);
+    router.delete('/user/all', controller.user.deleteAll);
     router.put('/user', controller.user.update);
     router.get('/user/:_id', controller.user.findOne);
     router.get('/user/', controller.user.findPage);
+
+    // cellecton operation
+    router.post('/cellecton/login', controller.cellecton.login);
+    router.get('/cellecton/:userId', controller.cellecton.find);
+    router.post('/cellecton', controller.cellecton.create);
+    router.delete('/cellecton/:_id', controller.cellecton.delete);
+    router.put('/cellecton', controller.cellecton.update);
+    router.get('/cellecton/:_id', controller.cellecton.findOne);
 };
